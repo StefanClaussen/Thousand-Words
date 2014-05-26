fef153fe7ace3fc4f2910a832134a90cd2f7bb9a
StefenClausen <stefanclaussen@me.com>
2014-05-26 11:20:19 +0100

    Complete ending lecture 260

---
+++ Thousand Words/TWPictureDataTransformer.h
@@ -0,0 +1,13 @@
+//
+//  TWPictureDataTransformer.h
+//  Thousand Words
+//
+//  Created by Stefan Claussen on 26/05/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TWPictureDataTransformer : NSValueTransformer
+
+@end
