d1b5f37a847107a24fa388a7e68a93f55fb4615f
StefenClausen <stefanclaussen@me.com>
2014-05-29 11:43:22 +0100

    Complete ending lecture 268

---
+++ Thousand Words/TWFiltersCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TWFiltersCollectionViewController.h
+//  Thousand Words
+
+#import <UIKit/UIKit.h>
+
+@class Photo;
+
+@interface TWFiltersCollectionViewController : UICollectionViewController
+
+@property (strong, nonatomic) Photo *photo;
+
+@end
--- Thousand Words/TWPhotoDetailViewController.m
+++ Thousand Words/TWPhotoDetailViewController.m
@@ -4,6 +4,7 @@
 
 #import "TWPhotoDetailViewController.h"
 #import "Photo.h"
+#import "TWFiltersCollectionViewController.h"
 
 @interface TWPhotoDetailViewController ()
 
@@ -39,16 +40,20 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"Filter Segue"])
+    {
+        if ([segue.destinationViewController isKindOfClass:[TWFiltersCollectionViewController class]])
+        {
+            TWFiltersCollectionViewController *targetViewController = segue.destinationViewController;
+            targetViewController.photo = self.photo;
+        }
+    }
 }
-*/
 
 - (IBAction)addFilterButtonPressed:(UIButton *)sender
 {
