1ded48b8af752008d96cd3531d5d7461f01f7f20
StefenClausen <stefanclaussen@me.com>
2014-05-29 13:58:05 +0100

    Complete ending lecture 271

--- Thousand Words/TWFiltersCollectionViewController.m
+++ Thousand Words/TWFiltersCollectionViewController.m
@@ -33,12 +33,43 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.filters = [[[self class] photoFilters] mutableCopy];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Helpers
+
++ (NSArray *)photoFilters
+{
+    CIFilter *sepia = [CIFilter filterWithName:@"CISepiaTone" keysAndValues: nil];
+    
+    CIFilter *blur = [CIFilter filterWithName:@"CIGaussianBlur" keysAndValues: nil];
+    
+    CIFilter *colorClamp = [CIFilter filterWithName:@"CIColorClamp" keysAndValues:@"inputMaxComponents", [CIVector vectorWithX:0.9 Y:0.9 Z:0.9 W:0.9], @"inputMinComponents", [CIVector vectorWithX:0.2 Y:0.2 Z:0.2 W:0.2], nil];
+    
+    CIFilter *instant = [CIFilter filterWithName:@"CIPhotoEffectInstant" keysAndValues: nil];
+    
+    CIFilter *noir = [CIFilter filterWithName:@"CIPhotoEffectNoir" keysAndValues: nil];
+    
+    CIFilter *vignette = [CIFilter filterWithName:@"CIVignetteEffect" keysAndValues: nil];
+    
+    CIFilter *colourControls = [CIFilter filterWithName:@"CIColorControls" keysAndValues:kCIInputSaturationKey, @0.5, nil];
+    
+    CIFilter *transfer = [CIFilter filterWithName:@"CIPhotoEffectTransfer" keysAndValues: nil];
+    
+    CIFilter *unsharpen = [CIFilter filterWithName:@"CIUnsharpMask" keysAndValues: nil];
+    
+    CIFilter *monochrome = [CIFilter filterWithName:@"CIColorMonochrome" keysAndValues: nil];
+    
+    NSArray *allFilters = @[sepia, blur, colorClamp, instant, noir, vignette, colourControls, transfer, unsharpen, monochrome];
+    
+    return allFilters;
 }
 
 #pragma mark - UICollectionView DataSource
